2facff2679494ae29a9f64f062e9c6b08580af33
anandhu-dev <54721685+anandhu-dev@users.noreply.github.com>
2019-09-01 13:48:37 +0530

    Update welcome_sql.py

--- tg_bot/modules/sql/welcome_sql.py
+++ tg_bot/modules/sql/welcome_sql.py
@@ -6,7 +6,7 @@ from tg_bot.modules.helper_funcs.msg_types import Types
 from tg_bot.modules.sql import SESSION, BASE
 
 DEFAULT_WELCOME = "Hey {first}, how are you?"
-DEFAULT_GOODBYE = "Nice knowing ya!"
+DEFAULT_GOODBYE = "പാവം പോയി രക്ഷപെടട്ടെ 😏😒☹️"
 
 
 class Welcome(BASE):
